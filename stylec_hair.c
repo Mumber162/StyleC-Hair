738c21d5c0428773fdcd60940fe03566277e5da4
Paulo de Tarso <pauloddtarso@gmail.com>
2023-08-20 23:40:14 -0300

    Create stylec_hair.c
    Tela inicial principal

---
+++ stylec_hair.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void menuPrincipal();
+int main()
+{
+	menuPrincipal();
+	return 0;
+}
+
+void menuPrincipal()
+{
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////\n");
+	printf("//                                                         //\n");
+	printf("//   = = = = = = = = = = = = = = = = = = = = = = = = = =   //\n");
+	printf("//   = = = =     STYLE-C HAIR: Cabeleireiros     = = = =   //\n");
+	printf("//   = = = = = = = = = = = = = = = = = = = = = = = = = =   //\n");
+	printf("//             Developed by @Mumber162 - Aug, 2023         //\n");
+	printf("//                                                         //\n");
+	printf("/////////////////////////////////////////////////////////////\n");
+	printf("//                                                         //\n");
+	printf("//    ===============  MENU PRINCIPAL ===============      //\n");
+	printf("//                                                         //\n");
+	printf("//     1. Modulo CLIENTES                                  //\n");
+	printf("//     2. Modulo AGENDAMENTOS                              //\n");
+	printf("//     3. Modulo SERVICOS                                  //\n");
+	printf("//     4. Modulo RELATORIOS                                //\n");
+	printf("//     5. Modulo FEEDBACK                                  //\n");
+	printf("//     5. SOBRE                                            //\n");
+	printf("//     0 - (SAIR do Programa)                              //\n");
+	printf("//                                                         //\n");
+	printf("/////////////////////////////////////////////////////////////\n");
+	printf("//     Digite a escolha desejada: ");
+	
+    //printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+}
