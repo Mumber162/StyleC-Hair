ae55a563688e49798e31927288be289891e57114
Paulo de Tarso <pauloddtarso@gmail.com>
2023-08-21 11:27:20 -0300

    Telas "Sobre" e "Time"
    Update stylec_hair.c
    + telaEquipe( );
    + telaSobre( );

--- stylec_hair.c
+++ stylec_hair.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 //------------------
-void menuPrincipal();
+void menuPrincipal(void);
+void telaSobre(void);
+void telaEquipe(void);
 void info();
 //------------------
 
@@ -16,17 +18,17 @@ void info()
 
 int main()
 {
-	info();
 	menuPrincipal();
 	return 0;
 }
 
-void menuPrincipal()
+void menuPrincipal(void)
 {
+	info();
 	printf("/////////////////////////////////////////////////////////////\n");
 	printf("//                                                         //\n");
 	printf("//   = = = = = = = = = = = = = = = = = = = = = = = = = =   //\n");
-	printf("//   = = = =     STYLE-C HAIR: Cabeleireiros     = = = =   //\n");
+	printf("//   = = = -     STYLE-C HAIR: Cabeleireiros     - = = =   //\n");
 	printf("//   = = = = = = = = = = = = = = = = = = = = = = = = = =   //\n");
 	printf("//       Developed by @Mumber162 - since Aug, 2023         //\n");
 	printf("//                                                         //\n");
@@ -39,11 +41,48 @@ void menuPrincipal()
 	printf("//     3. Modulo SERVICOS                                  //\n");
 	printf("//     4. Modulo RELATORIOS                                //\n");
 	printf("//     5. Modulo FEEDBACK                                  //\n");
-	printf("//     5. SOBRE                                            //\n");
+	printf("//     6. SOBRE                                            //\n");
+	printf("//     7. Time                                             //\n");
 	printf("//     0 - (SAIR do Programa)                              //\n");
 	printf("//                                                         //\n");
 	printf("/////////////////////////////////////////////////////////////\n");
 	printf("//     Digite a escolha desejada: ");
 	
     //printf("\t\t>>> Tecle <ENTER> para continuar...\n");
+}
+
+void telaSobre(void)
+{
+	info();
+	printf("/////////////////////////////////////////////////////////////\n");
+	printf("//                                                         //\n");
+	printf("//       ===============    SOBRE   ===============        //\n");
+	printf("//                                                         //\n");
+	printf("//  O StyleC Hair e um programa para um salao de cabelei-  //\n");
+	printf("//  reiro que visa simplificar a gestao de agendamentos,   //\n");
+	printf("//  horarios e servicos. Atraves de uma interface intui-   //\n");
+	printf("//  tiva, os funcionarios podem registrar agendamentos,    //\n");
+	printf("//  controlar horarios disponiveis, e acompanhar servicos. //\n");
+	printf("//  Isso otimiza as operacoes do salao, permitindo uma     //\n");
+	printf("//  maior dedicacao aos clientes e servicos de qualidade.  //\n");
+	printf("//                                                         //\n");
+	printf("/////////////////////////////////////////////////////////////\n");
+
+}
+
+void telaEquipe(void)
+{
+	info();
+	printf("/////////////////////////////////////////////////////////////\n");
+	printf("//                                                         //\n");
+	printf("//       ===============    TIME   ===============         //\n");
+	printf("//                                                         //\n");
+	printf("//    O StyleC Hair foi desenvolvido por:                  //\n");
+	printf("//                                                         //\n");
+	printf("//    Paulo de Tarso Medeiros Cavalcante                   //\n");
+	printf("//    E-mail: paulo.medeiros.703@ufrn.edu.br               //\n");
+	printf("//    Redes Sociais: @paulo_detars                         //\n");
+	printf("//    Git: https://github.com/Mumber162/StyleC-Hair.git    //\n");
+	printf("//                                                         //\n");
+	printf("/////////////////////////////////////////////////////////////\n");
 }
